4182dab402f747218b62779e9c2ac779d1c991fa
ydgydg <1005705069@qq.com>
2022-09-21 17:03:44 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup,find_packages
+setup(
+    name = 'DERcert',
+    version = '1.0',
+    install_requires = ['openssl','redis','requests',
+                        'matplotlib,'graphviz']
+)
