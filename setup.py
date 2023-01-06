101515aa331009f9081fde57e11b99f9d47a95a6
ydgydg <1005705069@qq.com>
2023-01-06 20:39:13 +0800

    delete

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup,find_packages
-setup(
-    name = 'DERcert',
-    version = '1.0',
-    install_requires = ['openssl','redis','requests',
-                        'matplotlib,'graphviz']
-)
